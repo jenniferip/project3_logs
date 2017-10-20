2e93dc55e95da11b5603aaf6a1f57d0f549df3dc
vagrant <vagrant@vagrant.vm>
2017-10-20 06:57:08 +0000

    Changed reporter.py for version 1 submission, altering how it answers question one and two

--- reporter.py
+++ reporter.py
@@ -4,12 +4,6 @@ the answers to the following three questions:
 1) What are the most popular three articles of all time?
 2) Who are the most popular article authors of all time?
 3) On which days did more than one percent of requests lead to errors?.
-
-NOTE: The creation and population of views and tables needs to only run once!
-If you run this code more than once you need to comment out all calls to
-functions that create tables or views in the if __name__ = __main__ block.
-There is a block of hashtags that signal where you should comment at the end
-of this file.
 """
 
 import re
@@ -17,97 +11,74 @@ import psycopg2
 
 
 def articles_and_authors():
-    """This function creates a view that called articles_and_authors by
-    joining the articles and authors tables on the articles.author column and
-    the authors.id column. This view will be useful in finding the top three
-    authors."""
-    cursor.execute("""create view articles_and_authors as select
-        articles.author, articles.title, articles.slug, articles.time,
-        articles.id, authors.name, authors.bio, authors.id as author_id from
-        articles, authors where articles.author = authors.id""")
+    """This function creates a view called articles_and_authors by selecting
+    the authors' name column and articles' title and slug column after joining
+    the articles and authors tables on the articles' author column and the
+    authors' id column. This view will be useful in finding the top three
+    articles and authors once it is combined with the log table.
+    """
+    cursor.execute("""create or replace view articles_and_authors as select
+        authors.name, articles.title, articles.slug from authors, articles
+        where articles.author = authors.id""")
     db_conn.commit()
 
 
-def update_log():
-    """This function creates and populates a table called updated_log to
-    include most of the information from the log table excluding the ip column
-    and including an additional column called article_slug that holds the slug
-    of the article the path in log leads to. This is in preparation to answer
-    question one."""
-    cursor.execute("""create table updated_log as select path, method, status,
-        time, id from log""")
-    db_conn.commit()
-    cursor.execute("alter table updated_log add article_slug text")
-    db_conn.commit()
-    cursor.execute("select path from updated_log")
-    paths = cursor.fetchall()
-    for p in paths:
-        pattern = re.compile('article/(.*)')
-        searched = pattern.search(p[0])
-        if searched is None:
-            cursor.execute("""update updated_log set article_slug = {} where
-                updated_log.path = '{}'""".format('NULL', p[0]))
-            db_conn.commit()
-        else:
-            a_slug = searched.group(1)
-            cursor.execute("""update updated_log set article_slug = '{}' where
-                updated_log.path = '{}'""".format(a_slug, p[0]))
-            db_conn.commit()
-
-
-def articles_and_logs():
-    """This function creates a view called articles_and_logs that combines the
-    articles table with the newly created updated_log table. It joins both
-    tables through the articles' slug column and the updated_log's
-    article_slug column. This view is useful in solving question one."""
-    cursor.execute("""create view articles_and_logs as select articles.author,
-        articles.title, articles.slug, articles.time, articles.id,
-        updated_log.method, updated_log.status, updated_log.article_slug,
-        updated_log.time as log_time, updated_log.id as log_id from articles,
-        updated_log where articles.slug = updated_log.article_slug""")
+def article_author_log():
+    """This function creates a view called article_author_log by combining the
+    articles_and_authors view, made in the function above, with the log table
+    on the articles_and_authors' slug column and the log's path column which
+    contains the slug of the article it leads to. This view has all of the
+    columns from the articles_and_authors view as well as all of the columns
+    from the log table. This will be useful in finding the top three articles
+    and authors.
+    """
+    cursor.execute("""create or replace view article_author_log as select *
+        from articles_and_authors left join log on log.path like
+        '%'||articles_and_authors.slug||'%'""")
     db_conn.commit()
 
 
 def top_three_articles():
     """This function finds the top three most popular articles of all time
-    using the view called articles_and_logs."""
-    cursor.execute("""select articles_and_logs.id, articles_and_logs.title,
-        count(articles_and_logs.method) as num_views from articles_and_logs
-        where articles_and_logs.method = 'GET' group by
-        articles_and_logs.title, articles_and_logs.id order by num_views desc
-        limit 3""")
+    using the view called article_author_log.
+    """
+    cursor.execute("""select title, count(method) as num_views from
+        article_author_log where status = '200 OK' group by title order by
+        num_views desc limit 3""")
     db_conn.commit()
     return cursor.fetchall()
 
 
 def top_three_authors():
-    """This function uses the articles_and_authors view and the
-    articles_and_logs view to find the top three most popular authors of all
-    time ie. the top three most viewed articles."""
-    cursor.execute("""select articles_and_authors.name,
-        count(articles_and_logs.method) as num_views from articles_and_logs,
-        articles_and_authors where articles_and_authors.title =
-        articles_and_logs.title and articles_and_logs.method = 'GET' group by
-        articles_and_authors.name order by num_views desc limit 3""")
+    """This function uses the article_author_log view to find the top three
+    most popular authors of all time ie. the top three most viewed authors
+    over all of their articles.
+    """
+    cursor.execute("""select name, count(method) as num_views from
+        article_author_log where status = '200 OK' group by name order by
+        num_views desc limit 3""")
     db_conn.commit()
     return cursor.fetchall()
 
 
 def requests_per_day():
-    """This function creates a table called requests_per_day with columns
+    """This function creates a view called requests_per_day with columns
     called date and total_requests. This is in preparation to answer question
-    three."""
-    cursor.execute("""create table requests_per_day as select date(time),
-        count(*) as total_requests from log group by date(time)""")
+    three.
+    """
+    cursor.execute("""create or replace view requests_per_day as select
+        date(time), count(*) as total_requests from log group by
+        date(time)""")
     db_conn.commit()
 
 
 def error_requests_per_day():
-    """This function creates a table called error_requests_per_day with
+    """This function creates a view called error_requests_per_day with
     columns called e_date and num_errors. This is in preparation to answer
-    question three."""
-    cursor.execute("""create table error_requests_per_day as select date(time)
-        as e_date, count(*) as num_errors from log where status =
+    question three.
+    """
+    cursor.execute("""create or replace view error_requests_per_day as select
+        date(time) as e_date, count(*) as num_errors from log where status =
         '404 NOT FOUND' group by e_date""")
     db_conn.commit()
 
@@ -117,7 +88,7 @@ def total_and_errors():
     requests_per_day table and the error_requests_per_day table on the
     requests_per_day.date column and the error_requests_per_day.e_date column.
     """
-    cursor.execute("""create view total_and_errors as select * from
+    cursor.execute("""create or replace view total_and_errors as select * from
         requests_per_day join error_requests_per_day on requests_per_day.date
         = error_requests_per_day.e_date""")
     db_conn.commit()
@@ -125,10 +96,12 @@ def total_and_errors():
 
 def days_with_too_many_errors():
     """This function finds the days in which more than one percent of its
-    requests resulted in an error and what the respective percentage was."""
-    cursor.execute("""select date, (cast(errors as float)/cast(total_requests
-        as float)) from total_and_errors where (cast(errors as float)/
-        cast(total_requests as float)) > 0.01""")
+    requests resulted in an error and what the respective percentage was.
+    """
+    cursor.execute("""select date,
+        (cast(num_errors as float)/cast(total_requests as float)) from
+        total_and_errors where
+        (cast(num_errors as float)/cast(total_requests as float)) > 0.01""")
     db_conn.commit()
     return cursor.fetchall()
 
@@ -137,18 +110,11 @@ if __name__ == '__main__':
     db_conn = psycopg2.connect(database='news')
     cursor = db_conn.cursor()
 
-    ##########################################################################
-    #  The lines sandwiched between the hashtag lines must be run the first  #
-    #  time you run this code, any time after that you will have to comment  #
-    #  them out.                                                             #
-    ##########################################################################
     articles_and_authors()
-    update_log()
-    articles_and_logs()
+    article_author_log()
     requests_per_day()
     error_requests_per_day()
     total_and_errors()
-    ##########################################################################
 
     top_3_articles = top_three_articles()
     top_3_authors = top_three_authors()
@@ -156,17 +122,28 @@ if __name__ == '__main__':
 
     db_conn.close()
 
-    results_file = open(“reporter_results.txt”, “w”)
-    results_file.write("""This first list gives the top three articles in our
-        database along with their respective number of views.""")
-    results_file.write(str(top_3_articles))
-    results_file.write("""Secondly, this lists the top three authors in our
-        database along with their respective total views for all articles they
-        have written.""")
-    results_file.write(str(top_3_authors))
-    results_file.write("""Finally, this last list tells us the days in which
-        more than one percent of its requests resulted in an error and also
-        tells us the proportion of errors with respect to the total requests.
-        """)
-    results_file.write(str(bad_days))
+    results_file = open('reporter_results.txt', 'w')
+
+    results_file.write("""These are the top three articles in our """ +
+                       """database with their respective number of views.
+                       \n""")
+    for article in top_3_articles:
+        results_file.write(str(article[0]) + ' - ' + str(article[1]) +
+                           ' views ' + '\n')
+    results_file.write('\n')
+
+    results_file.write("""Next, are the top three authors in our """ +
+                       """database with their respective total views """ +
+                       """over all articles they have written.\n""")
+    for author in top_3_authors:
+        results_file.write(str(author[0]) + ' - ' + str(author[1]) +
+                           ' views ' + '\n')
+    results_file.write('\n')
+
+    results_file.write("""Finally, these are the days in which more than """ +
+                       """one percent of its requests resulted in an """ +
+                       """errorthe percent of errors.\n""")
+    for day in bad_days:
+        results_file.write(str(day[0]) + ' - ' + str(day[1]*100) + '%' + '\n')
+
     results_file.close()
